4ab0931970cb3e088ce11dd54acceac6f924f8ab
Ronald Chancey <perchan@Knights.ucf.edu>
2022-04-14 09:30:59 -0400

    options menu prototype

---
+++ Assets/Scripts/MenuSripts/OptionsMenu.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public AudioMixer mixer;
+
+    [SerializeField]
+    Slider volumeSlider;
+
+    [SerializeField]
+    Dropdown graphicsDropdown;
+
+    [SerializeField]
+    Toggle cheatToggle;
+
+    void Awake()
+    {
+        //sets default playerprefs if they don't exist
+        if (!PlayerPrefs.HasKey("GameVolume"))
+        {
+            PlayerPrefs.SetFloat("GameVolume", 1);
+        }
+        if (!PlayerPrefs.HasKey("Graphics"))
+        {
+            PlayerPrefs.SetInt("Graphics", 3);
+        }
+        if (!PlayerPrefs.HasKey("TimeSlowCheat"))
+        {
+            PlayerPrefs.SetInt("TimeSlowCheat", 0);
+        }
+        LoadValues();   
+    }
+
+    public void SetVolume(float sliderValue)
+    {
+        //sets volume (logarithmic) and assigns volume playerpref
+        mixer.SetFloat("masterVolume", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("GameVolume", sliderValue);
+
+        Debug.Log("Volume: " + PlayerPrefs.GetFloat("GameVolume"));
+    }
+
+    public void SetGraphics(int qualityIndex)
+    {
+        //sets graphics and assigns graphics playerpref
+        QualitySettings.SetQualityLevel(qualityIndex);
+        PlayerPrefs.SetInt("Graphics", qualityIndex);
+
+        Debug.Log("Graphics: " + PlayerPrefs.GetInt("Graphics"));
+    }
+
+    public void SetTimeSlowCheat(bool cheatBool)
+    {
+        //assigns cheat playerpref (to be used as conditional in time slow mechanic)
+        PlayerPrefs.SetInt("TimeSlowCheat", cheatBool ? 1:0); //converts boolean to int
+    }
+
+    void LoadValues()
+    {
+        //loads object values
+        volumeSlider.value = PlayerPrefs.GetFloat("GameVolume");
+        graphicsDropdown.value = PlayerPrefs.GetInt("Graphics");
+        cheatToggle.isOn = PlayerPrefs.GetInt("TimeSlowCheat") == 1 ? true:false; //converts int to boolean
+
+        //sets each setting
+        SetVolume(volumeSlider.value);
+        SetGraphics(graphicsDropdown.value);
+        SetTimeSlowCheat(cheatToggle.isOn);
+
+        //loggies
+        Debug.Log("Volume: " + PlayerPrefs.GetFloat("GameVolume"));
+        Debug.Log("Graphics: " + PlayerPrefs.GetInt("Graphics"));
+        //Debug.Log("Time Slow Cheat: " + PlayerPrefs.GetInt("TimeSlowCheat") == 1 ? true:false); //error idk why :(
+    }
+}
