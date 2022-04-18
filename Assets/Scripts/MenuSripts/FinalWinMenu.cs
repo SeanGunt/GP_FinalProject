6c9628c350f7f091158c273eae20b1d0770c002f
SeanGunt <89537993+SeanGunt@users.noreply.github.com>
2022-04-17 20:42:50 -0400

    fixed win bug

--- Assets/Scripts/MenuSripts/FinalWinMenu.cs
+++ Assets/Scripts/MenuSripts/FinalWinMenu.cs
@@ -7,12 +7,12 @@ public class FinalWinMenu : MonoBehaviour
 {
     public void SwitchToMainMenu()
     {
+        GlobalSave.Instance.timeCoreScore = 0;
         SceneManager.LoadScene(0);
     }
 
     public void QuitGame()
     {
         Application.Quit();
-        GlobalSave.Instance.timeCoreScore = 0;
     }
 }
